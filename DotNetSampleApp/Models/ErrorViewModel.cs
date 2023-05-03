045cda1efa2e135036f0883f81b96b9b09b76de7
harik1 <harik1@paypal.com>
2023-05-03 15:17:26 +0530

    Add existing project files prior to the push to GitHub.

---
+++ DotNetSampleApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetSampleApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
